7f23c6ece43e9a699ff3e96e7f93d1e9eb0f4e1e
droge91 <droge91@outlook.com>
2024-02-12 15:46:40 -0800

    Now Initializes with a button

--- __init__.py
+++ __init__.py
@@ -5,14 +5,16 @@
 __license__ = ' MIT'
 __copyright__ = '2024, SEgroup'
 
-from calibre.customize import EditBookToolPlugin
+from calibre.customize import InterfaceActionBase
+from calibre_plugins.ebook_image import main
 
-
-class GrayscalePlugin(EditBookToolPlugin):
+class GrayScalePlugin(InterfaceActionBase):
 
     name = 'Image Grayscaler'
-    version = (1, 0, 0)
+    version = (1, 0, 1)
     author = 'SEgroup'
     supported_platforms = ['windows', 'osx', 'linux']
     description = 'First version of SEgroup plugin'
     minimum_calibre_version = (7, 0, 0)
+    icon = 'charcoal.png'
+    actual_plugin = 'calibre_plugins.ebook_image.main:GrayScaleAction'
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 
 
-# The base class that all tools must inherit from
 from calibre.gui2.actions import InterfaceAction
+from calibre.customize import InterfaceActionBase
+from PyQt5.Qt import QToolButton, QPixmap, QMenu,QIcon
 
 
-
-
-class GreyScaleACtion(InterfaceAction):
+class GrayScaleAction(InterfaceAction):
     name = 'grey-scale'
-
+    print("\nWHY\n")
     allowed_in_toolbar = True
 
     allowed_in_menu = True
-
-    def create_action(self):
+    description = "greyscale"
+    def genesis(self, for_toolbar=True):
+        print("\ngets\n")
+        self.is_library_selected = True
+        self.button = QToolButton()
+        pixmap = QPixmap('icons/charcoal.png')
+        if pixmap.isNull():
+            print("\nBROKEN\n")
+        self.button.setIcon(QIcon(pixmap))
+        self.button.setToolTip('greyscale books')
+        self.button.clicked.connect(self.greyscale)
+    def initialization_complete(self):
+        print("init complete")
+    def greyscale(self):
+        print("works")
