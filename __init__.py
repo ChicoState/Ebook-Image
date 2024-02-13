0868c5ab62be1f911fb4ee8139ccc0d1c2e5daf4
droge91 <droge91@outlook.com>
2024-02-12 16:31:57 -0800

    Added Button Icon

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ __license__ = ' MIT'
 __copyright__ = '2024, SEgroup'
 
 from calibre.customize import InterfaceActionBase
+#must be imported in order to access other files within directory
 from calibre_plugins.ebook_image import main
 
 class GrayScalePlugin(InterfaceActionBase):
@@ -16,5 +17,6 @@ class GrayScalePlugin(InterfaceActionBase):
     supported_platforms = ['windows', 'osx', 'linux']
     description = 'First version of SEgroup plugin'
     minimum_calibre_version = (7, 0, 0)
-    icon = 'charcoal.png'
+
+    #references class defined in main.py
     actual_plugin = 'calibre_plugins.ebook_image.main:GrayScaleAction'
--- main.py
+++ main.py
@@ -3,26 +3,29 @@
 from calibre.gui2.actions import InterfaceAction
 from calibre.customize import InterfaceActionBase
 from PyQt5.Qt import QToolButton, QPixmap, QMenu,QIcon
-
+import calibre.customize
+import calibre_plugins.ebook_image
 
 class GrayScaleAction(InterfaceAction):
-    name = 'grey-scale'
-    print("\nWHY\n")
-    allowed_in_toolbar = True
-
+    name = 'gray-scale'
+    allowed_in_toolbar = True 
     allowed_in_menu = True
-    description = "greyscale"
+    action_spec = ('Charcoal', 'charcoal.png', 'Grayscales Ebooks', None)
+    description = "grayscale"
+    #initializes the plugin
     def genesis(self, for_toolbar=True):
-        print("\ngets\n")
         self.is_library_selected = True
         self.button = QToolButton()
-        pixmap = QPixmap('icons/charcoal.png')
-        if pixmap.isNull():
-            print("\nBROKEN\n")
-        self.button.setIcon(QIcon(pixmap))
-        self.button.setToolTip('greyscale books')
-        self.button.clicked.connect(self.greyscale)
+        self.button.setToolTip('grayscale books')
+        self.button.clicked.connect(self.grayscale)
+        #sets icon for button
+        icon = get_icons('icons/charcoal.png')
+        if (icon.isNull()):
+            print("\n BROKEN\n")
+        self.qaction.setIcon(icon)
+    #always called after initialization
     def initialization_complete(self):
         print("init complete")
-    def greyscale(self):
+    #where the plugin funcionality is actually defined 
+    def grayscale(self):
         print("works")
