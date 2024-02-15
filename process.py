148e2cd18a54604e605d1bbb6aeff832a6aa9237
droge91 <droge91@outlook.com>
2024-02-14 17:15:05 -0800

    new epub compiled correctly, just needs to be saved in the correct place

--- process.py
+++ process.py
@@ -20,41 +20,32 @@ def GrayScale(db, book_title,QDialog):
             break
     #documentation recommends using copy_format_to instead but no idea where that is defined
     content = db.format_abspath(ID, 'EPUB')
-    images = []
     #using zipfile to load the epub
     #as far as I can tell you need to extract the files to a temporary directory to modify them
     #this seems clunky so if a workaround could be found that would be nice
     temp = tempfile.TemporaryDirectory()
     epub = zipfile.ZipFile(content)
+    epub_title = os.path.basename(content)
     epub.extractall(temp.name)
     for item in epub.infolist():
         if item.filename.endswith('.jpg'):
             with epub.open(item.filename) as page:
-                print('enters')
-                images.append(item.filename)
                 path = os.path.join(temp.name, item.filename)
                 #using pillow to convert the image to greyscale
                 image = Image.open(page)
-                grey = image.convert('L')
+                image = image.convert('L')
                 image.save(path)
-    with zipfile.ZipFile("new_epub", 'w') as new_epub:
+    with zipfile.ZipFile(epub_title, 'w') as new_epub:
         for root, sub, files in os.walk(temp.name):
             for i in range(len(sub)):
                 new_epub.mkdir(sub[i])
-                print (sub[i])
                 Meta = sub[0]
                 OEBPS = sub[1]
             for file in files:
-                if len(files) > 1:
-                    print('there')
+                if os.path.basename(root) == 'OEBPS' or os.path.basename(root) == 'META-INF':
                     path = os.path.join(root, file)
-                    print(sub)
-                    file = os.path.join(OEBPS, os.path.basename(file))
+                    file = os.path.join(os.path.basename(root), os.path.basename(file))
                 else:
-                    print('here')
                     path = os.path.join(root, file)
-                print(path)
-                print(file)
                 new_epub.write(str(path), file)
-
     temp.cleanup()
