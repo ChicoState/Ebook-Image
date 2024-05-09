3cd0ff68b3d34fba5e1e4422c59e2c0d2cb924c9
Angel Garcia <agarcia218@csuchico.edu>
2024-05-09 16:45:14 -0700

    resolves the linker issues

--- Charcoal/Charcoal/AZW3.cpp
+++ Charcoal/Charcoal/AZW3.cpp
@@ -13,6 +13,15 @@ namespace fs = std::experimental::filesystem;
 
 #pragma once
 
+std::string wstring_to_utf8(const std::wstring& wstr) {
+    if (wstr.empty()) return std::string();
+
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), nullptr, 0, nullptr, nullptr);
+    std::string strTo(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, nullptr, nullptr);
+    return strTo;
+}
+
 std::string safeCharPtrToString(const char* charPtr) {
     if (charPtr != nullptr) {
         return std::string(charPtr);
--- Charcoal/Charcoal/AZW3.h
+++ Charcoal/Charcoal/AZW3.h
@@ -3,14 +3,7 @@
 #include <Windows.h>
 #include <string>
 
-std::string wstring_to_utf8(const std::wstring& wstr) {
-    if (wstr.empty()) return std::string();
-
-    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), nullptr, 0, nullptr, nullptr);
-    std::string strTo(size_needed, 0);
-    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, nullptr, nullptr);
-    return strTo;
-}
+std::string wstring_to_utf8(const std::wstring& wstr);
 
 class mobi : public Library
 {
