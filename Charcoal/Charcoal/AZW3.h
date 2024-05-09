ff175027a1f613324c7ad79076be6f78c441f413
Angel Garcia <agarcia218@csuchico.edu>
2024-05-09 14:39:55 -0700

    the metadata for mobi and azw3 files can be read`

--- Charcoal/Charcoal/AZW3.h
+++ Charcoal/Charcoal/AZW3.h
@@ -1,6 +1,25 @@
 #pragma once
 #include "Library.h"
-class AZW3 : public Library
+#include <Windows.h>
+#include <string>
+
+std::string wstring_to_utf8(const std::wstring& wstr) {
+    if (wstr.empty()) return std::string();
+
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), nullptr, 0, nullptr, nullptr);
+    std::string strTo(size_needed, 0);
+    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, nullptr, nullptr);
+    return strTo;
+}
+
+class mobi : public Library
 {
+    public:
+        book add(PWSTR path);
 };
 
+class azw3 : public Library
+{
+public:
+    book add(PWSTR path);
+};
